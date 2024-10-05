a2df8687c436f6786db2d0ad072c6c2383347664
bryanambrose076 <68830798+bryanambrose076@users.noreply.github.com>
2024-10-04 18:56:42 -0700

    AdminClass Skeleton

---
+++ src/AdminClass.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class AdminClass extends UserClass {
+    private List<String> roles = new ArrayList<>();
+
+    public AdminClass(String username, char[] password, boolean isOTP, String firstName, String middleName, String lastName) {
+        super(username, password, isOTP, firstName, middleName, lastName);
+    }
+
+    public void inviteUser(String email, String role) {
+    }
+
+    public void resetUserAccount(UserClass user) {
+    }
+
+    public boolean deleteUserAccount(UserClass user) {
+        return false;
+    }
+
+    public List<UserClass> listUserAccounts() {
+        return new ArrayList<>();
+    }
+
+    public void addRole(UserClass user, String role) {
+    }
+
+    public void removeRole(UserClass user, String role) {
+    }
+
+    public List<String> getRoles() {
+        return roles;
+    }
+
+    public void addRole(String role) {
+        roles.add(role);
+    }
+
+    public void removeRole(String role) {
+        roles.remove(role);
+    }
+}
