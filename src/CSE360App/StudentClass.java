bc72e69faaf9315177f2fe0cfeee2229f363f638
bryanambrose076 <68830798+bryanambrose076@users.noreply.github.com>
2024-10-09 17:02:28 -0700

    Variable type and isadmin get/set

--- src/CSE360App/StudentClass.java
+++ src/CSE360App/StudentClass.java
@@ -4,36 +4,44 @@ import java.util.List;
 import java.util.Map;
 
 public class StudentClass extends UserClass {
-	boolean isAdmin;
-	String[] skills = {"Astah", "Eclipse", "Git", "Java", "JavaFX", "Scrum"};
-	Map<String, SkillLevel> skillMap = new HashMap<>();
+	private boolean isAdmin;
+	private static String[] skills = {"Astah", "Eclipse", "Git", "Java", "JavaFX", "Scrum"};
+	private Map<String, SkillLevel> skillMap = new HashMap<>();
 
 	public StudentClass(String username, char[] password, boolean isOTP, String firstName, String middleName,
 			String lastName, List<String> roles) {
 		super(username, password, isOTP, firstName, middleName, lastName, roles);
-		for(int i = 0; i < skills.length; i++) {
+		for (int i = 0; i < skills.length; i++) {
 			skillMap.put(skills[i], SkillLevel.INTERMEDIATE);
 		}
 	}
+
 	public StudentClass(String username, char[] password, boolean isOTP, String firstName, String middleName,
 			String lastName, List<String> roles, SkillLevel[] skillLevels) {
 		super(username, password, isOTP, firstName, middleName, lastName, roles);
-		for(int i = 0; i < skillLevels.length; i++) {
+		for (int i = 0; i < skillLevels.length; i++) {
 			skillMap.put(skills[i], skillLevels[i]);
 		}
 	}
+
 	public String getSkillLevel(String typeOfSkill) {
-		if(skillMap.containsKey(typeOfSkill)) {
+		if (skillMap.containsKey(typeOfSkill)) {
 			SkillLevel sk = skillMap.get(typeOfSkill);
 			return sk.toString();
-		}
-		else {
+		} else {
 			return "No skill";
 		}
 	}
-	
+
 	public void setSkillLevel(String typeOfSkill, SkillLevel skill) {
 		skillMap.put(typeOfSkill, skill);
 	}
 
+	public boolean getIsAdmin() {
+		return isAdmin;
+	}
+
+	public void setIsAdmin(boolean isAdmin) {
+		this.isAdmin = isAdmin;
+	}
 }
